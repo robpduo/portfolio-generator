dbd6def0a3ea6989e00d47aa82fa37be8d7e208b
robpduo <robpduo@Robs-MacBook-Pro.local>
2022-06-28 19:43:30 -0400

    new

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
-import { writeFile } from 'fs';
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const profileDataArgs = process.argv.slice(2);
+
 const [name, github] = profileDataArgs;
 
-const generatePage = (name, github) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
 
-    <body>
-      <h1>${name}</h1>
-      <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-  </html>
-  `;
-};
 
-writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
-  console.log(`Portfolio Complete! Check out index.html to see the output!`);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+  console.log('Portfolio complete! Check out index.html to see the output!');
 });
-
-https://courses.bootcampspot.com/courses/2193/pages/9-dot-2-5-generate-the-html-file?module_item_id=567800
